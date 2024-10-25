8591771ca038713686917d8253014938b8fc2c07
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-10-25 11:56:01 +0300

    created intro and lists python files

---
+++ intro.py
@@ -0,0 +1,21 @@
+name="Mercy Adhiambo  Ogalo"
+school="Jomo Kenyatta University of agrivulture and technology"
+year=2027
+mean_grade="B+"
+course="Business Information Technology"
+
+
+
+
+print(name)
+print(school)
+print(year)
+print(mean_grade)
+print(course)
+
+
+#inbuilt data types(4)
+#1 lists
+#2 tuples
+#3 dictionaries
+#4 sets
---
+++ lists.py
@@ -0,0 +1,5 @@
+people=["Mercy", "Linet", "Nancy", "Violet","Alfred","Doe"]
+
+#the index is just like an array
+print(people[3])
+print(len(people))
