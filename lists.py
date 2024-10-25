de6d5edc0b9ddca9643bcc55e4accecf0b12be6c
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-10-25 12:51:43 +0300

    updated the python files

--- lists.py
+++ lists.py
@@ -2,4 +2,18 @@ people=["Mercy", "Linet", "Nancy", "Violet","Alfred","Doe"]
 
 #the index is just like an array
 print(people[3])
+
+#length of the list
 print(len(people))
+
+index=len(people)-1
+
+print(people[index])
+
+#range of data in a lists
+print(people[0:6])
+
+people.insert(0, "Susan")
+people.remove("Doe")
+
+print(people)
---
+++ tuples.py
@@ -0,0 +1,5 @@
+people=("Mark", "Doe", "William","Jeff")
+
+#can not reasign the values unless you  change them into a lists
+
+print(len(people))
