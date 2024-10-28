aa74f0076525bb67d5f8fd8da925e01a8d4c1f23
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-10-28 11:47:41 +0300

    added a square and odd number conditions

--- condition.py
+++ condition.py
@@ -22,3 +22,29 @@ elif number < 0:
     
 else:
     print("Number is equal to 0")
+    
+    
+    
+    
+#program that checks if a number is even or odd
+
+newnum=5
+
+if newnum%2==0:
+    print("Number is even ")
+else:
+    print("Number is odd")
+    
+    
+#a program that calculates the square of any given value
+
+squarenum=int(input("Enter a number: "))
+
+if squarenum>0:
+    square=squarenum*squarenum
+    print(square)
+else:
+    print("Can't find square of 0")
+    
+    
+    name=string(input("Enter your name: "))
