01a5ae8ed646f28fb98f4f54df1e613e3a2b1440
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-10-30 00:34:01 +0300

    created a function and dictionary

--- functionprac.py
+++ functionprac.py
@@ -1,9 +1,10 @@
 
+students={}
 
-def students():
-    print("Hello world")
-    
-    
-    
-    
-students()
+students["name"]=str(input("Enter your name:  "))
+students["age"]=int(input("Enter your age:  "))
+students["course"]=str(input("Enter your course name:  "))
+
+print(f"Student name:  {students["name"]}")
+print(f"Student name:  {students["course"]}")
+print(f"Student name:  {students["age"]}")  
