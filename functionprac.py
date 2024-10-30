6965fea77cafa7346b9096592587248d5700a1f5
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-10-30 11:49:13 +0300

    updated and greated a new function

--- functionprac.py
+++ functionprac.py
@@ -1,10 +1,16 @@
 
-students={}
+   # students={}
 
-students["name"]=str(input("Enter your name:  "))
-students["age"]=int(input("Enter your age:  "))
-students["course"]=str(input("Enter your course name:  "))
+   # students["name"]=str(input("Enter your name:"))
+   # students["course"]=str(input("Enter your course name:"))
 
-print(f"Student name:  {students["name"]}")
-print(f"Student name:  {students["course"]}")
-print(f"Student name:  {students["age"]}")  
+   # print(f"Student name:{students["name"]}")
+   # print(f"Student course:{students["course"]}")
+   # print(f"Student age:{students["age"]}")  
+   
+def greet(name: "Guest"):
+   print(f"Hello, {name.upper()}")
+  
+   
+   greet()
+   greet("Joseph")
