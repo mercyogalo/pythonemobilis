df0475c0faa4eee89c3e13b2b050ef7d64f52a5e
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-10-30 12:30:11 +0300

    created a perimeter function

--- functions.py
+++ functions.py
@@ -1,22 +1,22 @@
 #a block of code that performs a specific function
 
-x = 60
-y = 70
-def add():
- print(f"the square of {x} is {x**2}") 
+#x = 60
+#y = 70
+#def add():
+# print(f"the square of {x} is {x**2}") 
 
     
     #from functions import add
     #this is used to import a function from other files
     
 
-def looping():
-        for num in range(15,50):
-            print(num)
+#def looping():
+  #      for num in range(15,50):
+  #          print(num)
             
-def names():
-    name=input("Enter your name: ")
-    print(f"Hello i'm {name}")
+#def names():
+ #   name=input("Enter your name: ")
+ #   print(f"Hello i'm {name}")
 
-names()
-        
+#names()
+
---
+++ learnfunctions.py
@@ -0,0 +1,12 @@
+#def avr(number1: int, number2: int, number3: int):
+  #  return (number1+ number2 + number3) / 3
+
+#average=avr(45,76,84)
+#print(average)
+
+
+def calculator(num1 : int , num2 : int):
+    print(f"Perimeter : {2*(num1+ num2)}")
+    print(f"Area: {num1 * num2}")
+    
+calculator(2,5)
